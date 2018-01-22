155be0395cb60fd53dd69e930d1ebb33d543f26e
kristianmagas <35660733+kristianmagas@users.noreply.github.com>
2018-01-22 20:47:21 +0100

    Update data.js

--- data.js
+++ data.js
@@ -44,10 +44,6 @@ var DataControl = function(){
 			user = "";
 		}
 		
-		if(mainDS.UsersTable.length > 0){
-			user = mainDS.UsersTable[0].Login;
-		}
-		
 		return $.grep(mainDS.UsersTable, function(x) { return x.Login == user}).length > 0;
 	};
 	
@@ -61,9 +57,6 @@ var DataControl = function(){
 			user = "";
 		}
 		
-		if(mainDS.UsersTable.length > 0){
-			user = mainDS.UsersTable[0].Login;
-		}
 		
 		return $.grep(mainDS.UsersTable, function(x) { return x.Login == user}).length > 0 ? $.grep(mainDS.UsersTable, function(x) { return x.Login == user})[0] : null;
 	};
